5b10e470d3e9ec259f37cbc2aa01bb267cf2da77
PhilipKlaass <pklaassen@arizona.edu>
2023-12-20 22:40:30 -0700

    COOOOOOde

--- mtf_curve_generator.py
+++ mtf_curve_generator.py
@@ -1,10 +1,12 @@
 import numpy as np
 import matplotlib as plt
+import random as rand
 
-#array that will hold our simulated pixel values
-
+#size of the Region of Interest or ROI/roi
 row= 15
 column = 15
+
+#array that will hold our simulated pixel values
 roi = np.zeros((row,column))
 
 #angle that the edge is tilted w.r.t. to vertical
@@ -17,6 +19,17 @@ height_of_pattern = (np.tan(theta))**(-1)
 # x-postion which the edge starts on the top of the roi
 edge_start = 15
 
+
+''''
+loop to iterate over roi; rows first then columns.
+if the column of a particular index is less then the edge start or 
+the edge start minus the height of patern then the index value is set to 
+1, this represents the pixels of the roi covered by the edge. Simlarly, the 
+columns greater than are the pixels not coveered and have values set too 12.
+
+
+!!change once ESF is made!!
+'''
 for y in range(0,row):
     
     for x in range(0, column):
@@ -25,6 +38,29 @@ for y in range(0,row):
         if x>edge_start or x > edge_start-y/height_of_pattern:
             roi[y,x]  = 12
 
-print(roi)
+
+'''
+Estimating MTF as a 6th degree polynomial
+'''
+def MTF(x):
+    c_1 = round(rand.uniform(-1,1)*(10**(-1)),4)
+    c_2 = round(rand.uniform(-1,1)*(10**(-2)),4)
+    c_3 = round(rand.uniform(-1,1)*(10**(-3)),4)
+    c_4 = round(rand.uniform(-1,1)*(10**(-5)),4)
+    c_5 = round(rand.uniform(-1,1)*(10**(-7)),4)
+    c_6 = round(rand.uniform(-1,1)*(10**(-9)),4)
+    sum_coefficients = c_1+((c_2)**2)**(1/2)+c_3+((c_4)**2)**(1/2)+c_5+((c_6)**2)**(1/2)
+    if sum_coefficients > 1:
+        c_0 = sum_coefficients-1
+    else:
+        c_0 =1- sum_coefficients
 
 
+'''
+plot mtf curve
+generate array of x-values
+find xvalue where mtf(x) is zero 
+yaxis == mtf(x)
+and plot
+'''
+def mtf_plt():
