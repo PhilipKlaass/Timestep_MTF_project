4ae394d1c5beac9a0e97eda6506c8e582e9f7c72
klaassphil <pklaassen@arizona.edu>
2023-12-29 20:10:46 -0700

    image plane

--- mtf_curve_generator.py
+++ mtf_curve_generator.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random as rand
 from PIL import Image
-import scipy as sci
+
 
 
 ''''
@@ -15,7 +15,7 @@ columns greater than are the pixels not coveered and have values set too 12.
 
 !!change once ESF is made!!
 '''
-def object_plane(theta, roi_height, roi_width):
+def make_object_plane(theta, roi_height, roi_width):
     #size of the Region of Interest or ROI/roi
     row= roi_height
     column = roi_width
@@ -29,9 +29,9 @@ def object_plane(theta, roi_height, roi_width):
     for y in range(0,row):
         for x in range(0, column):
             if x < edge_start or x < edge_start-np.tan((np.pi/180)*theta)*y:
-                roi[y,x] = 0
+                roi[y,x] = 100
             if x>edge_start or x > edge_start-np.tan((np.pi/180)*theta)*y:
-                roi[y,x]  = 255
+                roi[y,x]  = 224
 
     return roi
 
@@ -57,13 +57,28 @@ Estimating MTF as a 6th degree polynomial
 
     
 '''
+
+def make_image_plane(object_plane, size):
+    image_plane = np.zeros((size,size))
+    for x in range(0,size):
+        for y in range(0,size):
+            value = 0
+            for n in range(0,int(len(object_plane)/size)):
+                for m in range(0,int(len(object_plane)/size)):
+                    value =value + object_plane[n+x*int(len(object_plane)/size)][m+y*int(len(object_plane)/size)]
+            image_plane[x][y] = value/((len(object_plane)/size)**2)
+    return image_plane
+            
+
+
+
 '''
 plot mtf curve
 generate array of x-values
 find xvalue where mtf(x) is zero 
 yaxis == mtf(x)
 and plot
-'''
+
 def mtf_plt(step):
     zero = 10
     x_axis = []
@@ -76,14 +91,16 @@ def mtf_plt(step):
     plt.scatter(x_axis,y_axis)
     plt.show()
 
+'''
 
 def main():
-    edge_object = object_plane(10,1000,1000)
-    img = Image.fromarray(edge_object)
+    object_edge = make_object_plane(10,10000,10000)
+    img = Image.fromarray(object_edge)
     img.show()
-    
-
-
+    image_edge = make_image_plane(object_edge,1000)
+    img2 = Image.fromarray(image_edge)
+    img2.show()
+    print(image_edge)
     return
 
 main()
