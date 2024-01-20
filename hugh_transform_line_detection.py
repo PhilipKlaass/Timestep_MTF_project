661cc5798e7af48f0ed9e17be0749195483c83d8
PhilipKlaass <pklaassen@arizona.edu>
2024-01-19 19:03:12 -0700

    added plot

--- hugh_transform_line_detection.py
+++ hugh_transform_line_detection.py
@@ -37,22 +37,21 @@ def detect_edge(array, pixel_pitch):
 def find_line(point_coords):
     possible_lines = []
     h= 0
-    while h <25:
-        if len(possible_lines)<1000:
+    while h <500:
+        while len(possible_lines)<100:
             for element in point_coords:
                 r_max = (element[0]**2 + element[1]**2)**0.5
-                for i in range(int((100-len(possible_lines))/len(point_coords))):
-                    possible_lines.append((random.randint(1,100)*0.01*r_max,random.randint(-180,180)))
+                possible_lines.append((random.randint(1,100)*0.01*r_max,random.randint(-90,0)))
         for i in possible_lines:
             weight = 0 
             for j in point_coords:
                 x= j[0]
                 y= j[1]
-                theta = np.arctan(y/x)
+                theta = (np.pi/180)*np.arctan(y/x)
                 r_prime = x*np.cos(theta)+y*np.sin(theta)
-                if r_prime>0.85*i[0] and r_prime<1.15*i[1]:
+                if r_prime>0.90*i[0] and r_prime<1.1*i[1]:
                     weight += 1
-            if weight<5:
+            if weight<25:
                 possible_lines.remove(i)
 
 
@@ -60,13 +59,14 @@ def find_line(point_coords):
         h+=1
     return possible_lines
 
-def display_accumulator_space(list):
+def display_accumulator_space(list, point_coords):
     x =[]
     y =[]
     for i in list:
         x.append(i[0])
         y.append(i[1])
-    plt.axes
+    plt.scatter(x,y)
+    plt.show()
 
 
 
@@ -74,7 +74,8 @@ def display_accumulator_space(list):
 def main():
     array =get_array("csv.txt",6)
     edge_coords  = detect_edge(array, 1)
-    print(find_line(edge_coords))
+    lines = find_line(edge_coords)
+    display_accumulator_space(lines)
 
 
 
