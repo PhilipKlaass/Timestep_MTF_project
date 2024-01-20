f9502f7d289494c1ec1c7895ad52ca6b42d764d6
Klaassphil <pklaassen@arizona.edu>
2024-01-20 10:16:10 -0700

    added figure

--- hugh_transform_line_detection.py
+++ hugh_transform_line_detection.py
@@ -10,19 +10,19 @@ from matplotlib import cm
 
 def get_array(filename, size):
     f = open(str(filename), "r")
-    array = np.ones((size,size), np.int8)
+    array = np.ones((size,size))
     m =0
     for line in f:
         n=0
-        line_cleaned = line.strip("\n")
-        line_list = line_cleaned.split("  ")
+        line_cleaned = line.strip("   \n")
+        line_list = line_cleaned.split("   ")
         for i in line_list:
-            array[m][n] = int(i)
+            array[m][n] = int(float(i))
             n+=1
         m +=1
     return array
 
-def detect_edge_points(array, pixel_pitch):
+def detect_edge_points(array, threshold):
     size_i = len(array)-1
     size_j = len(array[0])-1
     dark_value = array[0][0]
@@ -30,56 +30,24 @@ def detect_edge_points(array, pixel_pitch):
     if dark_value>light_value:
         (dark_value,light_value) = (light_value,dark_value)
     average_value = (dark_value+light_value)/2
-    coords_for_edge = []
+    output_image = np.zeros((size_i,size_i), np.int8)
     for i in range(0,size_i):
         for j in range(0,size_j):
-            if 0.9*average_value<array[i][j] and array[i][j]< 1.1*average_value:
-                (x,y) = ((j+0.5)*pixel_pitch,(size_i-(i)+0.5)*pixel_pitch)
-                coords_for_edge.append((x,y))
-    return coords_for_edge
+            if (1-threshold)*average_value<array[i][j] and array[i][j]< (1+threshold)*average_value:
+                output_image[i][j]= 1
 
 
-def find_line(point_coords):
-    possible_lines = []
-    h= 0
-    theta_low = -70
-    theta_high = -50
-    while h <500:
-        while len(possible_lines)<1000-h*.05:
-            for element in point_coords:
-                r_max = (element[0]**2 + element[1]**2)**0.5
-                possible_lines.append((random.randint(1,100)*0.01*r_max,random.randint(theta_low,theta_high)))
-        for i in possible_lines:
-            weight = 0 
-            for j in point_coords:
-                x= j[0]
-                y= j[1]
-                theta = (np.pi/180)*np.arctan(y/x)
-                r_prime = x*np.cos(theta)+y*np.sin(theta)
-                if r_prime>0.90*i[0] and r_prime<1.1*i[1]:
-                    weight += 1
-            if weight<5:
-                possible_lines.remove(i)
-        h+=1
-    return possible_lines
 
-def display_accumulator_space(list):
-    x =[]
-    y =[]
-    for i in list:
-        x.append(i[0])
-        y.append(i[1])
-    plt.scatter(x,y)
-    plt.show()
+
+    return output_image
 
 
 
 
 def main():
-    array =get_array("csv.txt",6)
-    #edge_coords  = detect_edge_points(array, 1)
-    #lines = find_line(edge_coords)
-    #display_accumulator_space(lines)
+    array1 =get_array("csv.txt",100)
+    array  = detect_edge_points(array1, threshold =0.5)
+
 
 
     angles = np.linspace(-np.pi/2, np.pi/2,720, endpoint=False)
