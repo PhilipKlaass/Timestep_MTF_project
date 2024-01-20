6a5fff214bd692af45ad80a6d2a7fc9127c4b8f3
Klaassphil <pklaassen@arizona.edu>
2024-01-20 10:39:26 -0700

    fig

--- hugh_transform_line_detection.py
+++ hugh_transform_line_detection.py
@@ -52,32 +52,37 @@ def main():
 
     angles = np.linspace(-np.pi/2, np.pi/2,720, endpoint=False)
     h,theta, d = hough_line(array, theta= angles)
-    fig, axes = plt.subplots(1,3,figsize = (15,6))
+    fig, axes = plt.subplots(1,4,figsize = (15,6))
     ax = axes.ravel()
-    ax[0].imshow(array, cmap=cm.gray)
-    ax[0].set_title('Input image')
+
+    ax[0].imshow(array1)
+    ax[0].set_title("Input Image")
     ax[0].set_axis_off()
+
+    ax[1].imshow(array, cmap=cm.gray)
+    ax[1].set_title('Input image with edge points identified')
+    ax[1].set_axis_off()
 
     angle_step = 0.5 * np.diff(theta).mean()
     d_step = 0.5 * np.diff(d).mean()
     bounds = [np.rad2deg(theta[0] - angle_step),
               np.rad2deg(theta[-1] + angle_step),
               d[-1] + d_step, d[0] - d_step]
-    ax[1].imshow(np.log(1 + h), extent=bounds, cmap=cm.gray, aspect=1 / 1.5)
-    ax[1].set_title('Hough transform')
-    ax[1].set_xlabel('Angles (degrees)')
-    ax[1].set_ylabel('Distance (pixels)')
-    ax[1].axis('image')
+    ax[2].imshow(np.log(1 + h), extent=bounds, cmap=cm.gray, aspect=1 / 1.5)
+    ax[2].set_title('Hough transform')
+    ax[2].set_xlabel('Angles (degrees)')
+    ax[2].set_ylabel('Distance (pixels)')
+    ax[2].axis('image')
 
 
-    ax[2].imshow(array, cmap=cm.gray)
-    ax[2].set_ylim((array.shape[0], 0))
-    ax[2].set_axis_off()
-    ax[2].set_title('Detected lines')
+    ax[3].imshow(array, cmap=cm.gray)
+    ax[3].set_ylim((array.shape[0], 0))
+    ax[3].set_axis_off()
+    ax[3].set_title('Detected lines')
 
     for _, angle, dist in zip(*hough_line_peaks(h, theta, d)):
         (x0, y0) = dist * np.array([np.cos(angle), np.sin(angle)])
-        ax[2].axline((x0, y0), slope=np.tan(angle + np.pi/2))
+        ax[3].axline((x0, y0), slope=np.tan(angle + np.pi/2))
     plt.tight_layout()
     plt.show()
 
