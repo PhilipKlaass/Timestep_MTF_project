259c8ddbc680d8cfc4de4f984daaecb33b8d7cbf
PhilipKlaass <pklaassen@arizona.edu>
2024-03-04 10:36:30 -0700

    Added ESF Binning, Sav-golay filter, and LSF

--- esf-lsf_sampling.py
+++ esf-lsf_sampling.py
@@ -6,6 +6,10 @@ from scipy.optimize import curve_fit
 import scipy
 import scienceplots
 
+
+"""
+Convert from csv to numpy array
+"""
 def get_array(filename, size):
     f = open(str(filename), "r")
     array = np.ones((size,size))
@@ -15,18 +19,33 @@ def get_array(filename, size):
         line_cleaned = line.strip("   \n")
         line_list = line_cleaned.split("   ")
         for i in line_list:
-            array[m][n] = int(i)
+            array[m][n] = float(i)
             n+=1
         m +=1
     return array
+
+"""
+Summary:
+        Consider the array in R2, create a number lines perpendicular to the 
+        edge and a number(sampling_number) of vertical lines whose spacing is 
+        determined by the sampling_frequency. For each intersection of the 
+        vertical and perpendicular lines record the distance from the edge and
+        the intensity of the pixel in which the intersection ocurred.
+
+Variables:
+    - array: image
+    - theta and r: definition of our edge r= xcos(theta)+ysin(theta)
+    - smapling_frequency: samples per pixel width
+    - sample_number: number of samples taken on each side of edge 
+"""
 
 def get_esf(array, theta, r, sampling_frequency, sample_number):
     theta = -theta #change sign because the array indexes downwards and the math
                    #is easier when we consider our edge in the 1st quadrant
     x_intercept = r/np.cos(theta) - len(array)*0.5*np.tan(theta)
     esf = []
-    for y_edge in range(0,5*len(array)):
-        y_edge = y_edge/5 + 0.5 #change for spacing of perp lines, aka nbr of data points
+    for y_edge in range(0,len(array)):
+        y_edge = y_edge + 0.5 #change for spacing of perp lines, aka nbr of data points
         x_edge = -y_edge*np.tan(theta)+r/np.cos(theta)
         for i in range(sample_number):
             x_sample1 = x_intercept + i/sampling_frequency 
@@ -43,21 +62,74 @@ def get_esf(array, theta, r, sampling_frequency, sample_number):
                 esf.append((dist2,intensity2))
     return esf
 
-def average_esf(array):
-    values = [[],[],[]]
-    out = []
-    for i in range(len(array)):
-        if array[i][0] not in values[0]:
-            values[0].append(array[i][0])
-            values[1].append(array[i][1])
-            values[2].append(1)
-        else:
-            values[1][values[0].index(array[i][0])]+= array[i][1]
-            values[2][values[0].index(array[i][0])]+=1
-    for i in range(len(values[0])):
-        out.append((values[0][i],values[1][i]/values[2][i]))
-    return out
 
+"""
+Summary:
+
+Variable:
+    - esf_dist: sorted from min to max
+    - esf_intensity: list of values, identical indexes correspond to single
+                    points, aka esf_0 is (x,y) = (esf_dist[0],esf_intensity[0])
+
+    - binsize: measured as percentage of pixel width, determines the range of
+               distances over which intensity values will be averaged
+"""
+def esf_bin_smooth(esf_dist,esf_intensity,binsize):
+    minimum_dist = esf_dist[0]
+    binned_esf= []
+    number_of_bins = np.abs(int((2*minimum_dist)/binsize))
+    for i in range(number_of_bins):
+        tot_intensity = 0
+        k = 0
+        for j in range(len(esf_dist)):
+            if minimum_dist+(i-0.5)*binsize<esf_dist[j]<=minimum_dist+(i+0.5)*binsize:
+                tot_intensity+= esf_intensity[j]
+                k+= 1
+        binned_esf.append((minimum_dist+i*binsize, tot_intensity/k))
+
+    return binned_esf
+
+
+def get_derivative(x,y):
+    x_out=x
+    for i in range(1,len(x)-1):
+        y[i] = y[i+1]-y[i]
+    y_out = y
+    y_out = np.delete(y_out,0)
+    x_out = np.delete(x_out,0)
+    y_out = np.delete(y_out,len(y_out)-1)
+    x_out = np.delete(x_out,len(x_out)-1)
+    return x_out,y_out
+
+
+
+def get_esf2(array, theta, r, sampling_frequency, sample_number):
+    theta = -theta
+    esf = []
+    for row in range(0,len(array),6):
+        y1 = row+0.5
+        x1 = r/np.cos(theta) -y1*np.tan(theta)
+        col = floor(x1)
+        if col in range(len(array)):
+            intensity = array[row][col]
+            dist = col-x1
+            #(np.cos(theta)*col+np.sin(theta)*y1-r)
+            esf.append((dist,intensity))
+        col = col-sample_number
+        for i in range(2*sample_number):
+            col += i
+            if 0<col<len(array):
+                intensity = array[row][floor(col)]
+                dist = col-x1
+                #(np.cos(theta)*(col+0.5)+np.sin(theta)*y1-r)
+                esf.append((dist, intensity))
+    return esf
+
+
+
+'''
+Split a list of tuples into x and y arrays.
+'''
 def make_scatter(array):
     X = []
     Y = []
@@ -66,7 +138,9 @@ def make_scatter(array):
         Y.append(i[1])
     return (X,Y)
 
-
+"""
+Curve to fit our data, probably wont use
+"""
 def esf_function(x,a,b,c,D,x0):
 
     #return (x/np.abs(x))*(a*(np.abs(x)-x0)**(1/3)+b*(np.abs(x)-x0)**(1/5)+c*(np.abs(x)-x0)**(1/7))+D
@@ -76,40 +150,61 @@ def esf_function(x,a,b,c,D,x0):
 
 
 def main():
-    array = get_array("image0001.csv", 200)
+    array = get_array("image0006_corrected_(400,600)-(900,1100).csv", 200)
     #sampling_frequency in samples per pixel pitch
-    esf = get_esf(array, -0.04799655,89, 0.2,3)
-    X,Y = make_scatter(sorted(esf))
+    esf = get_esf(array, -0.0479966,40, 0.97,2)
+    X,Y =  make_scatter(sorted(esf))
+    binned_esf = esf_bin_smooth(X,Y, 0.15)
 
 
+    X_avg,Y_avg = make_scatter(binned_esf)
 
 
-
-    
-    averaged_esf = average_esf(sorted(esf))
-    X_avg,Y_avg = make_scatter(averaged_esf)
     plt.style.use(["science", "notebook", "grid"])
-    fig , ax = plt.subplots(1,2, figsize = (10,3.5))
-    ax[0].plot(X,Y,".", ms= 1.5)
-    #ax[0].set_xlabel("Distance")
-    ax[0].set_ylabel("Pixel Intensity Values")
-    ax[0].set_title("Oversampled ESF")
-    ax[1].plot(X_avg,Y_avg,".", ms= 1.5)
-    #ax[1].set_xlabel("Distance")
-    ax[1].set_title("Oversampled and Averaged ESF")
+    fig , ax = plt.subplots(2,3, figsize = (12,8))
+    ax[0][0].plot(X,Y,".", ms= 2)
+    ax[0][0].set_title("Oversampled")
 
-    X_interp = np.linspace(-15,15,200)
+    ax[0][1].plot(X_avg,Y_avg, ".", ms= 2)
+    ax[0][1].set_title("Binned into 0.1 pixel width")
+
+    X_interp = np.linspace(-5,5,200)
     Y_interp = scipy.interpolate.pchip_interpolate(X_avg, Y_avg, X_interp)
-    ax[1].plot(X_interp,Y_interp,'-', color = 'r', label= "PCHIP Interpolation", lw = 1.5)
+    Yhat = scipy.signal.savgol_filter(Y_interp,51,2)
+    ax[0][2].plot(X_interp,Y_interp,"--",label= "PCHIP Interpolation", lw= 0.75)
+    ax[0][2].set_title("Peicewise Cubic Interpolation")
+
+    ax[1][0].plot(X_interp,Yhat,'-', color = 'r',  lw = 1.5)
+    ax[1][0].set_title("Savitsky-Golay filter applied")
     
-    popt, pcov = curve_fit(esf_function, X,Y, p0=[10,10,10,120,0])
-    a_opt,b_opt,c_opt,D_opt, x0_opt = popt
-    x_model = np.linspace(min(X), max(X), len(X))
-    y_model = esf_function(x_model,a_opt,b_opt,c_opt,D_opt,x0_opt)
-    ax[1].plot(x_model,y_model,"--", color= "green", label = "Cubic curve fit", lw= 1.5)
-    ax[1].legend(loc ="upper left", fontsize = 10)
-    plt.figtext(0.5,0.02, "Distance to Edge [Pixel Pitch]", ha= "center", fontsize = 16 )
+    dy = np.gradient(Yhat,X_interp)
+    dx2,dy2 = get_derivative(X_interp,Y_interp)
+    ax[1][1].plot(dx2,dy2, ".")
+    ax[1][1].plot(X_interp,dy)
+    ax[1][1].set_title("Derivative using(np.gradient)")
+
+    xf = scipy.fft.fftfreq(101,0.1)
+    yf = scipy.fft.rfft(dy)
+    yf = np.abs(yf)
+    yfmax = max(yf)
+    for i in range(len(yf)):
+        yf[i]= yf[i]/yfmax
+    ax[1][2].plot(xf,yf,'-')
+    ax[1][2].set_title("|FFT| of Gradient")
+
+    #f = scipy.interpolate.PchipInterpolator(X_avg,Y_avg)
+    #x_interp2 = np.linspace(-16,16,400)
+    #y_interp2 = f(x_interp2)
+    #ax[1].plot(x_interp2,y_interp2, label = "poly")
+
+
+
+    #popt, pcov = curve_fit(esf_function, X,Y, p0=[10,10,10,120,0])
+    #a_opt,b_opt,c_opt,D_opt, x0_opt = popt
+    #x_model = np.linspace(min(X), max(X), len(X))
+    #y_model = esf_function(x_model,a_opt,b_opt,c_opt,D_opt,x0_opt)
+    #ax[1].plot(x_model,y_model,"--", color= "green", label = "Cubic curve fit", lw= 1.5)
+    #ax[1].legend(loc ="upper left", fontsize = 10)
+    plt.tight_layout(pad = 1.25)
     plt.show()
-    print(len(X))
 main()
-#print(esf_function(-15,10,10,10,120,0))
--- image_to_csv.py
+++ image_to_csv.py
@@ -2,13 +2,15 @@ import skimage as ski
 import matplotlib.pyplot as plt
 import numpy as np
 
-def open_image(filename):
-    img = ski.io.imread(filename)
-    print(img)
-    img_1 =img[350:550]
-    img_roi = np.transpose(img_1)
-    img_roi = img_roi[900:1100]
-    return img_roi
+def open_images(*filename):
+    out = ()
+    for i in filename:
+        img = ski.io.imread(i)
+        img_1 =img[400:600]
+        img_roi = np.transpose(img_1)
+        img_roi = img_roi[900:1100]
+        out = out + (img_roi,)
+    return out
 
 def save_as_csv(array,csv_filename):
     f = open(csv_filename,'a')
@@ -19,13 +21,33 @@ def save_as_csv(array,csv_filename):
             f.write('\n')
     f.close()
 
+def flatfield_correction(light, dark, image):
+    size = len(image)
+    light_dark = np.zeros((size,size))
+    image_dark = np.zeros((size,size))
+    tot = 0
+    for i in range(size):
+        for j in range(size):
+            tot+= light[i][j]-dark[i][j]
+            light_dark[i][j] = light[i][j]-dark[i][j]
+            image_dark[i][j] = image[i][j]- dark[i][j]
+    m = tot/(size**2)
+    image_dark*m
+    corrected_image = np.zeros((size,size))
+    for i in range(size):
+        for j in range(size):
+            corrected_image[i][j] = image_dark[i][j]/light_dark[i][j]
+    return corrected_image
+
+
 def main():
     
-    roi = open_image("image0001.bmp")
-    plt.imshow(roi,interpolation='nearest')
+    roi, light, dark = open_images("image0006.tiff","image0006_light.bmp", "dark.bmp")
+    corrected_roi = flatfield_correction(light,dark,roi)
+    plt.imshow(corrected_roi,interpolation='nearest')
     plt.colorbar()
     plt.title("Region of Interest")
     plt.show()
-    #save_as_csv(roi, "image0001.csv")
+    #save_as_csv(corrected_roi, "image0006_corrected_(400,600)-(900,1100).csv")
 main()
 
