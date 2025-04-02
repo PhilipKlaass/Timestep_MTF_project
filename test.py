5a1ffae6f16bef891e39d22665e4003bd3887115
PhilipKlaass <pklaassen@arizona.edu>
2025-04-02 12:45:11 -0700

    commit

--- test.py
+++ test.py
@@ -11,16 +11,8 @@ import simulate_edge
 import ISO
 import matplotlib.pyplot as plt
 import matplotlib
+import csv
 
-
-a = np.linspace(-10,10, 1000)
-b = np.ones(1000)
-
-aa ,bb  = MTF.lanczos_resampling(a, b, 3)
-
-plt.plot(aa,bb/max(bb))
-plt.plot(a,b/max(b))
-plt.show()
 
 N = 100
 A = simulate_edge.make_object_plane(16, 1000, 1000, 15,200)
@@ -35,9 +27,9 @@ light = np.ones((N,N))
 C = simulate_edge.convolve(normal,B)
 
 for i in range(100):
-    dark += np.random.normal(0,0.05,(N,N))/100
-    light += np.random.normal(0,0.05,(N,N))/100
-    C += np.random.normal(0,0.05,(N,N))/100
+    dark += np.random.normal(0,0.01,(N,N))/100
+    light += np.random.normal(0,0.01,(N,N))/100
+    C += np.random.normal(0,0.01,(N,N))/100
 dark = dark + np.ones((N,N))*15
 light = light + np.ones((N,N))*235
 
@@ -70,6 +62,8 @@ lines = MTF.hough_transform(edge_points,threshold,False)
 
 r,theta = lines[0][1],lines[0][0]
 
+theta = np.arctan(1/m)
+
 mm = 1/np.tan(theta)
 
 plt.imshow(CC)
@@ -78,29 +72,55 @@ plt.plot([N/2,-N/m+N/2],[0,N],'r')
 plt.plot([N/2,-N/mm+N/2],[0,N],'b')
 plt.show()
 
-erf_x,erf_y = MTF.get_esf(CC, theta, r, "total")
+erf_x,erf_y = MTF.get_esf(CC*255, theta, r, "total")
+#erf_y = ISO.get_esf(CC, m)
 
+#erf_x = np.linspace(-50, 50,400)
+
+fig = plt.figure()
 plt.scatter(erf_x, erf_y)
 plt.show()
 
-erf_x_resampled,erf_y_resampled = MTF.lanczos_resampling(erf_x,erf_y,3)
+#erf_x_resampled,erf_y_resampled = MTF.lanczos_resampling(erf_x,erf_y,3)
+#erf_x_resampled,erf_y_resampled = MTF.bin_esf(erf_x,erf_y,0.25)
+erf_x_resampled,erf_y_resampled = MTF.bin_esf(erf_x,erf_y,0.25)
 
+
+fig = plt.figure()
 plt.scatter(erf_x_resampled,erf_y_resampled)
 plt.show()
 
-lsf_x, lsf_y = MTF.get_derivative(erf_x_resampled,erf_y_resampled)
-M = max(lsf_y)
-for i in range(len(lsf_y)):
-    lsf_y[i] = lsf_y[i]/M
+lsf_x, lsf_y = MTF.get_lsf(erf_x_resampled,erf_y_resampled,1)
+
+fig = plt.figure()
+plt.scatter(lsf_x, lsf_y)
+plt.show()   
+
 
 mtf_x,mtf_y = MTF.FFT(lsf_x,lsf_y)
 
 print("ISO slope is %f" % m)
 print("Hugh line slope is %f" % mm)
 
+mtfdh = []
+mtffreq = []
 
+with open("MTFDH.csv",newline = '') as csvfile:
+    file = csv.reader(csvfile)
+    for row in file:
+        mtfdh.append(float(row[0]))
+with open("MTFFREQ.csv",newline = '') as csvfile:
+    file = csv.reader(csvfile)
+    for row in file:
+        for i in range(len(row)):
+            mtffreq.append(float(row[i]))
 
-plt.plot(mtf_x,mtf_y)
+fig = plt.figure()
+plt.plot(mtf_x,mtf_y,label = "Our code")
+plt.plot(mtffreq,mtfdh,'r',label = "MTF_dh")
 plt.xlim((0,1))
+plt.title("MTF")
+plt.xlabel("Spatial Frequency (per pixel)")
+plt.legend()
+plt.savefig('MTF_compare', dpi = 400)
 plt.show()
-
